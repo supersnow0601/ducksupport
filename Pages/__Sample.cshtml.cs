e9b3b0ac6c7c6cffd598a68be517c69cdef8222e
twdeveloper <twdeveloper@outlook.com>
2020-01-16 21:28:17 +0800

    v1

---
+++ Pages/__Sample.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using isRock.Web.Core.Razor;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace __NameSpace__.Pages
+{
+    public class SampleModel : isPageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        [PageMethod]
+        public static object CalBmi(dynamic para)
+        {
+            int height = para.height;
+            int weight = para.weight;
+
+            float H = para.height;
+            H = H / 100;
+            return weight / (H * H);
+        }
+    }
+}
---
+++ Pages/__SampleLineBot.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using isRock.Web.Core.Razor;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace __NameSpace__.Pages
+{
+    public class SampleLineBot : isPageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        [PageMethod]
+        public static object PushMessage(dynamic para)
+        {
+            try
+            {
+                string Token = para.fieldToken;
+                string userID = para.fieldUserId;
+                string Message = para.fieldMessage;
+
+                var bot = new isRock.LineBot.Bot(Token);
+                //push text
+                bot.PushMessage(userID, Message);
+                //push sticker
+                bot.PushMessage(userID, 1, 2);
+                return true;
+            }
+            catch (System.Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
